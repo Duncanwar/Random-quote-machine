1c0bc3d73a9d8c80eae514c0479fc76027ed78ed
Duncanwar <semunda9@gmail.com>
2020-06-10 17:07:02 +0200

    react app structure

---
+++ random-quote-machine/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Machine from './components/machine'
+
+const  App = () => {
+  return (
+    <div className="App">
+ <Machine/>
+    </div>
+  );
+}
+
+export default App;
---
+++ random-quote-machine/src/components/machine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const machine =(props)=> {
+    return (
+        <div>
+           Hello 
+        </div>
+    );
+}
+
+export default machine;
