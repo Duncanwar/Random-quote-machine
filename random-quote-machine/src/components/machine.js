5da9fcff3dd62e87652e6adaed1fb5fe3bfc700a
Duncanwar <semunda9@gmail.com>
2020-06-10 17:37:02 +0200

    using hooks in react in quote machine

--- random-quote-machine/src/components/machine.js
+++ random-quote-machine/src/components/machine.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {index} from './quote'
 
-const machine =(props)=> {
+const Machine =()=> {
+    let [text,setText]= useState([]);
+    let [author,setAuthor] = useState([])
+    let [state, setState] = useState([])
+
+   const handleQuote = () => {
+       state = index()
+       setText(state.text)
+       setAuthor(state.author)
+       console.log(state.i)
+    }
+
+    useEffect(()=>{
+handleQuote()
+
+    },[])
+
     return (
-        <div>
-           Hello 
-        </div>
+        <div id="quote-box">
+        <div id="text"><span>{text}</span></div>
+    <div id="author">{author}</div>
+        <div id="tweet-quote">
+        <a  href="https://www.twitter.com/intent/tweet">tweet</a>
+    </div>
+        <div id="new-quote" >
+        <button id="new" onClick={handleQuote} >New Quote</button>
+    </div>
+    </div>
     );
 }
 
-export default machine;
+export default Machine;
